7ef7ebfe6396ef511594068161dbfdd2bf0843d2
ArcherGu <649601376@qq.com>
2021-06-02 08:23:06 +0800

    feat: 404 page

---
+++ pages/[...all].vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="no-page text-center">
+        <h1>Error 404</h1>
+    </div>
+</template>
+
+<style>
+.no-page {
+    h1 {
+        font-size: 5rem;
+    }
+}
+</style>

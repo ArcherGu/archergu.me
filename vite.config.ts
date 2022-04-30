91a9b02f9ef7bc547c5c0a6ddeebf536c781bdad
ArcherGu <649601376@qq.com>
2022-04-30 15:49:08 +0800

    chore: dependencies update

--- vite.config.ts
+++ vite.config.ts
@@ -1,99 +1,105 @@
 import { resolve } from 'path';
 import fs from 'fs-extra';
 import { defineConfig } from 'vite';
-import ViteVue from '@vitejs/plugin-vue';
-import VitePages from 'vite-plugin-pages';
-import ViteMarkdown from 'vite-plugin-md';
-import ViteComponents from 'vite-plugin-components';
-import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons';
-import ViteWindiCSS from 'vite-plugin-windicss';
-import matter from 'gray-matter';
+import Vue from '@vitejs/plugin-vue';
+import Pages from 'vite-plugin-pages';
+import Components from 'unplugin-vue-components/vite';
+import Markdown from 'vite-plugin-md';
 import Prism from 'markdown-it-prism';
+import LinkAttributes from 'markdown-it-link-attributes';
+import WindiCSS from 'vite-plugin-windicss';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+
+import matter from 'gray-matter';
 import anchor from 'markdown-it-anchor';
-import markdownAttr from 'markdown-it-link-attributes';
 import { slugify } from './scripts/slugify';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0'
+  resolve: {
+    alias: {
+      '@/': `${resolve(__dirname, 'src')}/`,
     },
-    resolve: {
-        alias: {
-            '@/': `${resolve(__dirname, 'src')}/`,
-        },
-    },
-    optimizeDeps: {
-        include: [
-            'vue',
-            'vue-router',
-            '@vueuse/core',
-            'dayjs',
-            'dayjs/plugin/localizedFormat',
-        ],
-    },
-    plugins: [
-        ViteVue({
-            include: [/\.vue$/, /\.md$/],
+  },
+  plugins: [
+    Vue({
+      include: [/\.vue$/, /\.md$/],
+      reactivityTransform: true,
+    }),
+
+    Pages({
+      extensions: ['vue', 'md'],
+      pagesDir: 'pages',
+      extendRoute(route) {
+        const path = resolve(__dirname, route.component.slice(1));
+
+        if (!path.includes('projects.md')) {
+          const md = fs.readFileSync(path, 'utf-8');
+          const { data } = matter(md);
+          route.meta = Object.assign(route.meta || {}, { frontmatter: data });
+        }
+
+        return route;
+      },
+    }),
+
+    Markdown({
+      wrapperComponent: 'Blog',
+      wrapperClasses: 'prose m-auto',
+      headEnabled: true,
+      markdownItOptions: {
+        quotes: '""\'\'',
+      },
+      markdownItSetup(md) {
+        md.use(Prism);
+        md.use(anchor, {
+          slugify,
+          permalink: anchor.permalink.linkInsideHeader({
+            symbol: '#',
+            placement: 'before',
+          })
+        });
+
+        md.use(LinkAttributes, {
+          matcher: (link: string) => /^https?:\/\//.test(link),
+          attrs: {
+            target: '_blank',
+            rel: 'noopener',
+          },
+        });
+      },
+    }),
+
+    Components({
+      extensions: ['vue', 'md'],
+      dts: true,
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        IconsResolver({
+          componentPrefix: '',
         }),
+      ],
+    }),
 
-        VitePages({
-            extensions: ['vue', 'md'],
-            pagesDir: 'pages',
-            extendRoute(route) {
-                const path = resolve(__dirname, route.component.slice(1));
+    Icons({
+      defaultClass: 'inline',
+      defaultStyle: 'vertical-align: sub;',
+    }),
 
-                if (!path.includes('projects.md')) {
-                    const md = fs.readFileSync(path, 'utf-8');
-                    const { data } = matter(md);
-                    route.meta = Object.assign(route.meta || {}, { frontmatter: data });
-                }
-
-                return route;
-            },
-        }),
-
-        ViteMarkdown({
-            wrapperComponent: 'Blog',
-            wrapperClasses: 'prose m-auto',
-            headEnabled: true,
-            markdownItOptions: {
-                quotes: '""\'\'',
-            },
-            markdownItSetup(md) {
-                md.use(Prism);
-                md.use(anchor, {
-                    slugify,
-                    permalink: anchor.permalink.linkInsideHeader({
-                        symbol: '#',
-                        placement: 'before',
-                    })
-                });
-
-                md.use(markdownAttr, {
-                    pattern: /^https?:/,
-                    attrs: {
-                        target: '_blank',
-                        rel: 'noopener',
-                    },
-                });
-            },
-        }),
-
-        ViteComponents({
-            extensions: ['vue', 'md'],
-            customLoaderMatcher: path => path.endsWith('.md'),
-            customComponentResolvers: ViteIconsResolver({
-                componentPrefix: '',
-            }),
-        }),
-
-        ViteIcons(),
-
-        ViteWindiCSS({
-            safelist: 'prose prose-sm m-auto'.split(' '),
-            preflight: {
-                enableAll: true,
-            },
-        }),
-    ]
+    WindiCSS({
+      safelist: 'prose prose-sm m-auto'.split(' '),
+      preflight: {
+        enableAll: true,
+      },
+    }),
+  ],
+  optimizeDeps: {
+    include: [
+      'vue',
+      'vue-router',
+      '@vueuse/core',
+      'dayjs',
+      'dayjs/plugin/localizedFormat',
+    ],
+  },
 });

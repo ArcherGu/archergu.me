bb1a91400a4fccf0ce517bde57b2ca5aa406a51d
archergu <649601376@qq.com>
2026-06-10 14:33:45 +0800

    fix: align footer备案 layout

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,17 @@ useHead({
 
 <template>
   <NavBar />
-  <main class="px-7 py-10">
-    <router-view />
+  <main class="site-main flex flex-col px-7 py-10">
+    <div class="flex-1">
+      <router-view />
+    </div>
     <Footer />
   </main>
 </template>
+
+<style scoped>
+.site-main {
+  min-height: calc(100vh - 5rem);
+  min-height: calc(100dvh - 5rem);
+}
+</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,9 @@
 <script setup lang='ts'>
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
-const route = useRoute()
-const isHome = computed(() => route.path === '/')
 const showIcp = import.meta.env.VITE_ENABLE_ICP === 'true'
 </script>
 
 <template>
-  <div
-    class="mt-10 mb-6 prose m-auto opacity-70" :class="{
-      'text-center': isHome,
-    }"
-  >
+  <div class="mt-10 prose m-auto opacity-70 text-center">
     <span class="text-sm">
       <a
         target="_blank"
@@ -21,10 +13,25 @@ const showIcp = import.meta.env.VITE_ENABLE_ICP === 'true'
       2021 © Archer Gu
       <template v-if="showIcp">
         <br>
-        <a
-          href="https://beian.miit.gov.cn/"
-          target="_blank"
-        >浙ICP备2026040669号-1</a>
+        <span class="inline-flex flex-col sm:flex-row sm:gap-3">
+          <a
+            href="https://beian.miit.gov.cn/"
+            target="_blank"
+          >浙ICP备2026040669号-1</a>
+          <a
+            class="inline-flex items-center justify-center gap-1"
+            href="https://beian.mps.gov.cn/#/query/webSearch?code=33028102001924"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img
+              alt=""
+              class="inline-block h-4 w-auto !m-0"
+              src="/beian-police.png"
+            >
+            <span>浙公网安备33028102001924号</span>
+          </a>
+        </span>
       </template>
     </span>
   </div>
